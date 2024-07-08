bcba34314a38baedfad44ddadb2366e1a77f8d6a
arch-hill <ahill120@umd.edu>
2024-07-08 15:38:37 -0400

    Create ArrayOoB.java

---
+++ ArrayOoB.java
@@ -0,0 +1,6 @@
+public class ArrayIndexOutOfBounds {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(arr[10]);  // Array index out of bounds
+    }
+}
